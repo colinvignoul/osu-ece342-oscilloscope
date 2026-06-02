00eb662860702ad5d8b05d6269db10b699bd13ca
Colin Vignoul <cjvignoul@gmail.com>
2026-06-01 20:30:22 -0700

    Update encoder behavior

--- code/src/core/rotary_decoder.cpp
+++ code/src/core/rotary_decoder.cpp
@@ -14,10 +14,10 @@ namespace {
 // Index is previous AB state in the high bits and current AB state in the low
 // bits. Invalid two-bit jumps contribute 0, which filters bounce/skipped steps.
 constexpr std::int8_t kTransitionTable[16] = {
-    0, -1, 1, 0,
-    1, 0, 0, -1,
-    -1, 0, 0, 1,
-    0, 1, -1, 0,
+     0,  1, -1,  0,
+    -1,  0,  0,  1,
+     1,  0,  0, -1,
+     0, -1,  1,  0
 };
 constexpr std::int8_t kTransitionsPerDetent = 4;
 
